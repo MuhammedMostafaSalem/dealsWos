f57fcb1ba1b50496027166721e2cfbd12e042161
mohammed <marwansalem164@gmail.com>
2025-08-06 13:22:17 +0300

    Add slider component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Slider from "./components/slider/Slider"
 import Header from "./components/utils/header"
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
       <Header />
+      <Slider />
     </>
   )
 }
